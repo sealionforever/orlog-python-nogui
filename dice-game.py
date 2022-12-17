df14278d924e43e97fbb66aabd33ab69a803bb0b
sealionforever <basdewolf@live.be>
2022-12-17 23:47:11 +0100

    Added match case for orlog values

--- dice-game.py
+++ dice-game.py
@@ -9,18 +9,42 @@ player2_score = 0
 for i in range(10):
 
     # Generate random numbers between 1 and 6 for each player.
-    player1_value = random.randint(1, 6)
-    player2_value = random.randint(1, 6)
+    player1_roll = random.randint(1, 5)
+    player2_roll = random.randint(1, 5)
+
+    match player1_roll:
+        case 1:
+            player1_value = "Axe"
+        case 2:
+            player1_value = "Arrow"
+        case 3:
+            player1_value = "Helmet"
+        case 4:
+            player1_value = "Shield"
+        case 5:
+            player1_value = "Hand"
+    
+    match player2_roll:
+        case 1:
+            player2_value = "Axe"
+        case 2:
+            player2_value = "Arrow"
+        case 3:
+            player2_value = "Helmet"
+        case 4:
+            player2_value = "Shield"
+        case 5:
+            player2_value = "Hand"
 
     # Display the values
     print("Player 1 rolled: ", player1_value)
     print("Player 2 rolled: ", player2_value)
 
     # Selection: based on comparison of the values, take the appropriate path through the code.
-    if player1_value > player2_value:
+    if player1_roll > player2_roll:
         print("player 1 wins.")
         player1_score = player1_score + 1  # This is how we increment a variable
-    elif player2_value > player1_value:
+    elif player2_roll > player1_roll:
         print("player 2 wins")
         player2_score = player2_score + 1
     else:
