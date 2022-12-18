113971b6876b2afba5f52c1ea9483c42628d4d84
sealionforever <basdewolf@live.be>
2022-12-18 23:53:32 +0100

    Added basic god favor tokens
    Added todo for small issue with particular edge case

--- dice-game.py
+++ dice-game.py
@@ -5,6 +5,10 @@ import random
 player1_health = 20
 player2_health = 20
 
+# Initialise player god coins to 0
+player1_tokens = 0
+player2_tokens = 0
+
 # Repeat everything in this block 10 times
 for i in range(10):
 
@@ -17,6 +21,10 @@ for i in range(10):
     player2_axe_damage = 0
     player1_arrow_damage = 0
     player2_arrow_damage = 0
+    player1_gain_tokens = 0
+    player2_gain_tokens = 0
+    player1_steal_tokens = 0
+    player2_steal_tokens = 0
 
     # Resolve rolled actions
     match player1_roll:
@@ -30,10 +38,13 @@ for i in range(10):
             player1_value = "Helmet"
             player2_axe_damage -= 1
         case 4:
-            player1_value = "Shield"
+            player1_value = "Shield+"
             player2_arrow_damage -= 1
+            player2_gain_tokens += 1
         case 5:
-            player1_value = "Hand"
+            player1_value = "Hand+"
+            player1_steal_tokens += 1
+            player1_gain_tokens += 1
     
     match player2_roll:
         case 1:
@@ -46,10 +57,13 @@ for i in range(10):
             player2_value = "Helmet"
             player1_axe_damage -= 1
         case 4:
-            player2_value = "Shield"
+            player2_value = "Shield+"
             player1_arrow_damage -= 1
+            player2_gain_tokens += 1
         case 5:
-            player2_value = "Hand"
+            player2_value = "Hand+"
+            player2_steal_tokens += 1
+            player2_gain_tokens += 1
 
     # Summarize damage
     if player1_axe_damage < 0:
@@ -61,10 +75,24 @@ for i in range(10):
     if player2_arrow_damage < 0:
         player2_arrow_damage = 0
     
+    # Deal Damage
     player1_health -= player2_axe_damage + player2_arrow_damage
     player2_health -= player1_axe_damage + player1_arrow_damage
 
-    # Display the values
+    # Gain tokens
+    player1_tokens += player1_gain_tokens
+    player2_tokens += player2_gain_tokens
+
+    # TODO: Issue when one has 0 tokens and both steal...
+    # Steal tokens
+    if player2_tokens > 0:
+        player2_tokens -= player1_steal_tokens
+        player1_tokens += player1_steal_tokens
+    if player1_tokens > 0:
+        player1_tokens -= player2_steal_tokens
+        player2_tokens += player2_steal_tokens
+
+    # Display the damage values
     if player1_axe_damage + player1_arrow_damage > 0:
         player1_damage_value = "some"
     else:
@@ -75,8 +103,10 @@ for i in range(10):
     else:
         player2_damage_value = "no"
 
-    print("Player 1 rolled: ", player1_value, " and takes", player1_damage_value, "damage. Health remaining: ", player1_health)
-    print("Player 2 rolled: ", player2_value, " and takes", player2_damage_value, "damage. Health remaining: ", player2_health)
+    print("Player 1 rolled: ", player1_value, " and takes", player1_damage_value, "damage.")
+    print("Player 2 rolled: ", player2_value, " and takes", player2_damage_value, "damage.")
+    print("Player 1:\n    health ", player1_health, "\n    tokens ", player1_tokens)
+    print("Player 2:\n    health ", player2_health, "\n    tokens ", player2_tokens)
 
     # Selection: based on comparison of the values, take the appropriate path through the code.
     if player1_health > player2_health:
@@ -90,4 +120,6 @@ for i in range(10):
 
 print("### Game Over ###")
 print("Player 1 health:", player1_health)
+print("Player 1 tokens:", player1_tokens)
 print("Player 2 health:", player2_health)
+print("Player 2 tokens:", player2_tokens)
